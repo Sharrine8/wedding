d8836a4057d313bfa533c44414df38e009d106db
Sharrine8 <65632872+Sharrine8@users.noreply.github.com>
2025-04-17 22:32:03 -0500

    fix: vite config error

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === "production" ? "/code-jam_react/" : "/",
+    base: mode === "production" ? "/wedding/" : "/",
     plugins: [react()],
     server: {
       port: 5000,
